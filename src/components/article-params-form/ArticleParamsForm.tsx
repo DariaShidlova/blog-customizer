975416bf50dd4fcdca431872f1fec07cbd787227
Daria Shidlova <daryashidlova@yandex.ru>
2025-03-12 21:50:01 +0300

    внесены изменения

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -5,133 +5,126 @@ import { Select } from 'src/ui/select';
 import { Separator } from 'src/ui/separator';
 import clsx from 'clsx';
 import {
-  ArticleStateType,
-  fontColors,
-  fontFamilyOptions,
-  backgroundColors,
-  contentWidthArr,
-  defaultArticleState,
-  fontSizeOptions,
+	ArticleStateType,
+	fontColors,
+	fontFamilyOptions,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontSizeOptions,
 } from 'src/constants/articleProps';
 import { useOutsideClickClose } from '../../ui/select/hooks/useOutsideClickClose';
 import styles from './ArticleParamsForm.module.scss';
 import { RadioGroup } from 'src/ui/radio-group';
 
 type ArticleParamsFormProps = {
-  isOpen: boolean;
-  onClose: () => void;
-  onApply: (settings: ArticleStateType) => void;
-  onReset: () => void;
-  currentSettings: ArticleStateType;
-  onChange: (newState: boolean) => void;
+	isOpen: boolean;
+	onClose: () => void;
+	onApply: (settings: ArticleStateType) => void;
+	onReset: () => void;
+	currentSettings: ArticleStateType;
+	onChange: (newState: boolean) => void;
 };
 
 export const ArticleParamsForm = ({
-  isOpen,
-  onClose,
-  onApply,
-  onReset,
-  currentSettings,
-  onChange,
+	isOpen,
+	onClose,
+	onApply,
+	onReset,
+	currentSettings,
+	onChange,
 }: ArticleParamsFormProps) => {
-    // Локальное состояние для черновика настроек
-  const [draftSettings, setDraftSettings] = useState(currentSettings);
-   // Реф для сайдбара
-  const sidebarRef = useRef<HTMLDivElement>(null);
+	// Локальное состояние для черновика настроек
+	const [draftSettings, setDraftSettings] = useState(currentSettings);
+	// Реф для сайдбара
+	const sidebarRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (isOpen) setDraftSettings(currentSettings);
-  }, [isOpen, currentSettings]);
+	useEffect(() => {
+		if (isOpen) setDraftSettings(currentSettings);
+	}, [isOpen, currentSettings]);
 
-  // Закрытие при клике вне области
-  useOutsideClickClose({
-    isOpen,
-    rootRef: sidebarRef,
-    onClose,
-    onChange,
-  });
+	// Закрытие при клике вне области
+	useOutsideClickClose({
+		isOpen,
+		rootRef: sidebarRef,
+		onClose,
+		onChange,
+	});
 
-  // Обработчик изменений полей
-  const handleSettingChange = (type: keyof ArticleStateType) => 
-    (value: typeof draftSettings[typeof type]) => {
-      setDraftSettings(prev => ({ ...prev, [type]: value }));
-    };
+	// Обработчик изменений полей
+	const handleSettingChange =
+		(type: keyof ArticleStateType) =>
+		(value: (typeof draftSettings)[typeof type]) => {
+			setDraftSettings((prev) => ({ ...prev, [type]: value }));
+		};
 
-    // Отправка формы
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    onApply(draftSettings);
-  };
+	// Отправка формы
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply(draftSettings);
+	};
 
-  // Сброс формы
-  const handleFormReset = () => {
-    setDraftSettings(defaultArticleState);
-    onReset();
-  };
+	// Сброс формы
+	const handleFormReset = () => {
+		setDraftSettings(defaultArticleState);
+		onReset();
+	};
 
-  return (
-    <>
-    {/* кнопка-стрелка */}
-      <ArrowButton isOpen={isOpen} onClick={() => onChange(!isOpen)} />
-        {/* Сайдбар с настройками */}
-      <aside
-        ref={sidebarRef}
-        className={clsx(styles.container, {
-          [styles.container_open]: isOpen,
-        })}>
-        <form
-          className={styles.form}
-          onSubmit={handleSubmit}
-          onReset={handleFormReset}>
-          <Select
-            title="Шрифт"
-            selected={draftSettings.fontFamilyOption}
-            options={fontFamilyOptions}
-            onChange={handleSettingChange('fontFamilyOption')}
-          />
-          <Separator />
-		  <RadioGroup
-            title="Размер шрифта"
-            name="font-size"
-            options={fontSizeOptions}
-            selected={draftSettings.fontSizeOption}
-            onChange={handleSettingChange('fontSizeOption')}
-          />
-          <Separator />
-          <Select
-            title="Цвет шрифта"
-            selected={draftSettings.fontColor}
-            options={fontColors}
-            onChange={handleSettingChange('fontColor')}
-          />
-          <Separator />
-          <Select
-            title="Цвет фона"
-            selected={draftSettings.backgroundColor}
-            options={backgroundColors}
-            onChange={handleSettingChange('backgroundColor')}
-          />
-          <Separator />
-          <Select
-            title="Ширина контента"
-            selected={draftSettings.contentWidth}
-            options={contentWidthArr}
-            onChange={handleSettingChange('contentWidth')}
-          />
-          <div className={styles.bottomContainer}>
-            <Button
-              title="Сбросить"
-              type="clear"
-              htmlType="reset"
-            />
-            <Button
-              title="Применить"
-              type="apply"
-              htmlType="submit"
-            />
-          </div>
-        </form>
-      </aside>
-    </>
-  );
+	return (
+		<>
+			{/* кнопка-стрелка */}
+			<ArrowButton isOpen={isOpen} onClick={() => onChange(!isOpen)} />
+			{/* Сайдбар с настройками */}
+			<aside
+				ref={sidebarRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleFormReset}>
+					<Select
+						title='Шрифт'
+						selected={draftSettings.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={handleSettingChange('fontFamilyOption')}
+					/>
+					<Separator />
+					<RadioGroup
+						title='Размер шрифта'
+						name='font-size'
+						options={fontSizeOptions}
+						selected={draftSettings.fontSizeOption}
+						onChange={handleSettingChange('fontSizeOption')}
+					/>
+					<Separator />
+					<Select
+						title='Цвет шрифта'
+						selected={draftSettings.fontColor}
+						options={fontColors}
+						onChange={handleSettingChange('fontColor')}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={draftSettings.backgroundColor}
+						options={backgroundColors}
+						onChange={handleSettingChange('backgroundColor')}
+					/>
+					<Separator />
+					<Select
+						title='Ширина контента'
+						selected={draftSettings.contentWidth}
+						options={contentWidthArr}
+						onChange={handleSettingChange('contentWidth')}
+					/>
+					<div className={styles.bottomContainer}>
+						<Button title='Сбросить' type='clear' htmlType='reset' />
+						<Button title='Применить' type='apply' htmlType='submit' />
+					</div>
+				</form>
+			</aside>
+		</>
+	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState, ArticleStateType } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
@@ -11,46 +14,46 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-  const [currentSettings, setCurrentSettings] = useState(defaultArticleState); //текущие примененные настройки
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); //состояние видимости сайтбара
+	const [currentSettings, setCurrentSettings] = useState(defaultArticleState); //текущие примененные настройки
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false); //состояние видимости сайтбара
 
-  const handleApply = (newSettings: ArticleStateType) => {
-    setCurrentSettings(newSettings); //сохранение новых настроек
-    setIsSidebarOpen(false); //закрытие сайтбара
-  };
+	const handleApply = (newSettings: ArticleStateType) => {
+		setCurrentSettings(newSettings); //сохранение новых настроек
+		setIsSidebarOpen(false); //закрытие сайтбара
+	};
 
-  const handleReset = () => {
-    setCurrentSettings(defaultArticleState); //сброс к первоначальному состоянию
-    setIsSidebarOpen(false);
-  };
+	const handleReset = () => {
+		setCurrentSettings(defaultArticleState); //сброс к первоначальному состоянию
+		setIsSidebarOpen(false);
+	};
 
-  return (
-    <main
-      className={clsx(styles.main)}
-      style={
-        {
-          '--font-family': currentSettings.fontFamilyOption.value,
-          '--font-size': currentSettings.fontSizeOption.value,
-          '--font-color': currentSettings.fontColor.value,
-          '--container-width': currentSettings.contentWidth.value,
-          '--bg-color': currentSettings.backgroundColor.value,
-        } as CSSProperties
-      }>
-      <ArticleParamsForm
-        isOpen={isSidebarOpen} //текущее состояние
-        onClose={() => setIsSidebarOpen(false)} //закрытие
-        onApply={handleApply} //подтверждение изменений
-        onReset={handleReset} //сброс настроек
-        currentSettings={currentSettings} //актуальные настройки
-		onChange={(newState) => setIsSidebarOpen(newState)} //изменение состояния
-      />
-      <Article />
-    </main>
-  );
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': currentSettings.fontFamilyOption.value,
+					'--font-size': currentSettings.fontSizeOption.value,
+					'--font-color': currentSettings.fontColor.value,
+					'--container-width': currentSettings.contentWidth.value,
+					'--bg-color': currentSettings.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm
+				isOpen={isSidebarOpen} //текущее состояние
+				onClose={() => setIsSidebarOpen(false)} //закрытие
+				onApply={handleApply} //подтверждение изменений
+				onReset={handleReset} //сброс настроек
+				currentSettings={currentSettings} //актуальные настройки
+				onChange={(newState) => setIsSidebarOpen(newState)} //изменение состояния
+			/>
+			<Article />
+		</main>
+	);
 };
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
